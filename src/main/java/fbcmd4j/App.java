65377e8209d031d090e444cca6f9d77a19054cd7
JorgePedraza2019 <jorge_luis.2008@hotmail.com>
2019-11-21 01:20:26 -0600

    Archivos de la actividad 10

--- src/main/java/fbcmd4j/App.java
+++ src/main/java/fbcmd4j/App.java
@@ -3,12 +3,35 @@
  */
 package fbcmd4j;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import facebook4j.Facebook;
+import facebook4j.FacebookException;
+import facebook4j.FacebookFactory;
+import facebook4j.User;
+
 public class App {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
     public String getGreeting() {
         return "Hello world.";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
+        Facebook facebook = new FacebookFactory().getInstance();
+
+        User me = null;
+        try {
+            me = facebook.getMe();
+        } catch (FacebookException e) {
+            e.printStackTrace();
+        }
+
+        logger.error(me.getId());
+        logger.error(me.getName());
+        logger.error(me.getEmail());
+
     }
 }
